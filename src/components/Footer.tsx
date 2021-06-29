9e685bb22b745d1a85c8f53b315b7d77ae6cd313
Dov Rine <dov@techimpact.org>
2021-06-29 15:28:49 -0700

    exercise 16: answer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div data-testid="Footer" className="Footer">
+      &copy; 2021 XYZ Corporation, all rights reserved
+    </div>
+  );
+}
+
+export { Footer };
+export default Footer;
