319c18109b943ba861c072a661c6d660faea140f
Dov Rine <dov@techimpact.org>
2021-06-29 15:38:51 -0700

    exercise 17

--- src/components/Cart.test.tsx
+++ src/components/Cart.test.tsx
@@ -1,14 +1,19 @@
 import { render } from "@testing-library/react";
 
-import Cart from './Cart'
+import Cart from "./Cart";
 
-
-describe('Cart', () => {
-    it('exists', () => {
-        expect(typeof Cart).toBe('function')
-    })
-    it('has class Cart', () => {
-        const ui = render(<Cart />)
-        expect(ui.getByTestId('Cart')).toHaveClass('Cart')
-    })
-})
+describe("Cart", () => {
+  it("exists", () => {
+    expect(typeof Cart).toBe("function");
+  });
+  it("has class Cart", () => {
+    const ui = render(<Cart />);
+    expect(ui.getByTestId("Cart")).toHaveClass("Cart");
+  });
+  it("starts empty", () => {
+    const ui = render(<Cart />);
+    expect(ui.queryAllByTestId("CartItem").length).toBe(0);
+    expect(ui.getByLabelText("subtotal")).toHaveTextContent("0.00");
+    expect(ui.getByLabelText("item count")).toHaveTextContent('0');
+  });
+});
