ac2d4911e57bae5fade2816508b822dfc857a2cb
Dov Rine <dov@techimpact.org>
2021-06-29 15:39:13 -0700

    exercise 17: answer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,8 @@
 function Cart() {
-  return <div data-testid="Cart" className="Cart"></div>;
+  return <div data-testid="Cart" className="Cart">
+    <div aria-label="subtotal">0.00</div>
+    <div aria-label="item count">0</div>
+  </div>;
 }
 export { Cart };
 export default Cart;
