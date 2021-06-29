db3b8750888fedcdba6510413fce21f79265d69c
Dov Rine <dov@techimpact.org>
2021-06-29 14:26:13 -0700

    exercise 11: answer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ function Header() {
   return (
     <header data-testid="Header" className="Header">
       <Link to="/">XYZ Corporation</Link>
+      <Link to="/cart">Add to Cart</Link>
     </header>
   );
 }
