f0098c9187b6e0214b87b81c9bbaf85ef2148afd
Dov Rine <dov@techimpact.org>
2021-06-29 13:08:37 -0700

    exercise-8-answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import ProductList from './components/ProductList'
+
 import './App.scss';
+
+import products from './data/products.json'
 
 function App() {
   return (
     <div className="App">
       XYZ Corporation
+      <ProductList products={products} />
     </div>
   );
 }
