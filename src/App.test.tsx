76ea432e92fe539f797afb32b2bd426c27c90a53
Dov Rine <dov@techimpact.org>
2021-06-29 10:44:42 -0700

    exercise 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Product from './components/Product'
 
 test('renders', () => {
   render(<App />);
   const companyName = screen.getByText(/XYZ Corporation/i);
   expect(companyName).toBeInTheDocument();
 });
+
+describe('Product', () => {
+  it('exists', () => {
+    expect(typeof Product).toBe('function')
+  })
+  it.todo('has all required fields')
+  it.todo('has a working add to cart button')
+})
