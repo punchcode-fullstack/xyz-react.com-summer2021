a3019c3eea5ec6ac93b73199e37fcb4f95617b51
Dov Rine <dov@techimpact.org>
2021-06-29 15:01:39 -0700

    exercise 15

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 
+import App from "./App";
 
-test('renders', () => {
-  render(<App />);
+function renderUi() {
+  return render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+}
+test("renders", () => {
+  renderUi();
   const companyName = screen.getByText(/XYZ Corporation/i);
   expect(companyName).toBeInTheDocument();
 });
 
-describe('App', () => {
-  it('shows the Shop by default', () => {
-    const ui = render(<App />)
-    const shop = ui.getByTestId('Shop')
-    expect(shop).toBeInTheDocument()
-  })
-})
+describe("App", () => {
+  it("shows the Shop by default", () => {
+    const ui = renderUi();
+    const shop = ui.getByTestId("Shop");
+    expect(shop).toBeInTheDocument();
+  });
+  it("navigates correctly between pages", () => {
+    const ui = renderUi();
+    const cartLink = ui.getByText("Cart");
 
+    cartLink.click();
+    const cart = ui.getByTestId("Cart");
+    expect(cart).toBeInTheDocument();
+
+    const shopLink = ui.getByText("XYZ Corporation");
+    shopLink.click();
+    const shop = ui.getByTestId("Shop");
+    expect(shop).toBeInTheDocument();
+  });
+});
