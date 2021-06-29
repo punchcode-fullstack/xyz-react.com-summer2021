0680c265f052ac7fff787ba89b42471190aad1cd
Dov Rine <dov@techimpact.org>
2021-06-29 11:09:23 -0700

    exercise 3

--- src/components/Product.test.tsx
+++ src/components/Product.test.tsx
@@ -1,14 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import Product from "./Product";
+import products from "../data/products.json";
 
 describe("Product", () => {
   it("exists", () => {
     expect(typeof Product).toBe("function");
   });
   it('has class of "Product"', () => {
-      const ui = render(<Product />)
-      expect(ui.getByTestId('Product')).toHaveClass('Product')
+      const product1 = products[0]
+    const ui = render(<Product {...product1} />);
+    expect(ui.getByTestId("Product")).toHaveClass("Product");
   });
-  it.todo("has all required fields");
-  it.todo("has a working add to cart button");
+  it("has all required fields", () => {
+    const product1 = products[0];
+    const ui = render(<Product {...product1} />);
+    const product = ui.getByTestId("Product");
+    expect(product).toHaveTextContent(product1.name);
+    expect(product).toHaveTextContent(String(product1.price));
+  });
+  it.todo("has an add to cart button");
 });
