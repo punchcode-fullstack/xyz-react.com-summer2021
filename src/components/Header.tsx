79d6000e9c18f7e7f00b5850e3bbb70c11d92bd6
Derrique B <derriquebaluyut@gmail.com>
2021-06-30 16:27:08 -0700

    add Header.scss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Header.scss'
 function Header() {
   return (
-    <header data-testid="Header" className="Header">
-      <Link to="/">XYZ Corporation</Link>
-      <Link to="/cart">Cart</Link>
+    <header data-testid='Header' className='Header'>
+      <nav>
+        <Link to='/' className='logo'>
+          XYZ Corporation
+        </Link>
+        <Link to='/cart' className='cart'>
+          Cart <FontAwesomeIcon icon={faShoppingCart} />
+          <div>0</div>
+        </Link>
+      </nav>
     </header>
-  );
+  )
 }
-export { Header };
-export default Header;
+export { Header }
+export default Header
