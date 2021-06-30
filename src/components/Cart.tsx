f537d04f909022f66c58254158d748a1f9839379
Dov Rine <dov@techimpact.org>
2021-06-30 11:39:06 -0700

    exercise 20: answer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,20 +1,33 @@
-import {CartItemType} from './CartItem.types'
-import CartItem from './CartItem'
+import { CartItemType } from "./CartItem.types";
+import CartItem from "./CartItem";
 
 interface CartType {
   cartItems: CartItemType[];
 }
 
-function Cart({cartItems=[]}: CartType) {
-  return <div data-testid="Cart" className="Cart">
-    <div aria-label="subtotal">0.00</div>
-    <div aria-label="item count">0</div>
-    <div className="cart-items">
-      {cartItems?.map(
-        (item) => <CartItem key={item.id} {...item} />
-      )}
+function Cart({ cartItems = [] }: CartType) {
+  return (
+    <div data-testid="Cart" className="Cart">
+      <div aria-label="subtotal">
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(
+          cartItems?.reduce(
+            (subtotal: number, item: CartItemType) =>
+              subtotal + parseFloat(String(item.price)),
+            0
+          )
+        )}
+      </div>
+      <div aria-label="item count">{cartItems?.length ?? 0}</div>
+      <div className="cart-items">
+        {cartItems?.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 }
 export { Cart };
 export default Cart;
