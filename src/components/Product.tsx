9c57bc0cd35c085b63fb1acc0cc74f2ed710f835
Derrique B <derriquebaluyut@gmail.com>
2021-07-01 09:02:48 -0700

    add product scss

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,20 +1,34 @@
-import type { Product as ProductType} from "./Product.types";
-
+import type { Product as ProductType } from './Product.types'
+import './Product.scss'
 interface ProductIface {
-  product: ProductType;
-  addItemToCart: (item: ProductType) => void;
+  product: ProductType
+  addItemToCart: (item: ProductType) => void
 }
 
 function Product({ product, addItemToCart }: ProductIface) {
   return (
-    <div data-testid="Product" className="Product">
-      <div>{product.id}</div>
-      <div>{product.name}</div>
-      <div>{product.price}</div>
-      <button onClick={() => addItemToCart(product)}>Add to Cart</button>
+    <div data-testid='Product' className='Product'>
+      {/* <div>{product.id}</div> */}
+      <div
+        style={{
+          background: `url("${product.thumbnail}")`,
+          height: '250px',
+          width: '300px',
+          backgroundPosition: '45% 50%',
+          backgroundSize: '280%',
+        }}
+        className='bg'
+      ></div>
+      <div className='info'>
+        <div>{product.name}</div>
+        <div>
+          <div className='price'>{product.price}</div>
+          <button onClick={() => addItemToCart(product)}>Add to Cart</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export { Product };
-export default Product;
+export { Product }
+export default Product
