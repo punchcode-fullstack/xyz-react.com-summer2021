a343fc0ef8f159441da7b04042be1cf4b4af1c32
Dov Rine <dov@techimpact.org>
2021-07-01 16:55:28 -0700

    fix broken styles from merge conflict resolution

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,20 +2,25 @@ import { CartItemType } from "./CartItem.types";
 import CartItem from "./CartItem";
 import useCart from "../hooks/useCart";
 
+import "./Cart.scss";
+
 function Cart() {
   const { cartItems, calculateCart } = useCart();
   return (
     <div data-testid="Cart" className="Cart">
-      <div aria-label="subtotal">
-        {new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format(calculateCart())}
-      </div>
-      <div aria-label="item count">{cartItems?.length ?? 0}</div>
+      <header>
+        <h1 className="subtotal" aria-label="subtotal">
+          Your Cart{" "}
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(calculateCart())}
+        </h1>
+        <h1 aria-label="item count">(Items: {cartItems?.length ?? 0})</h1>
+      </header>
       <div className="cart-items">
-        {cartItems?.map((item: CartItemType) => (
-          <CartItem key={item.id} {...item} />
+        {cartItems?.map((item: CartItemType, i: number) => (
+          <CartItem key={item.id} {...item} itemIndex={i} />
         ))}
       </div>
     </div>
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { CartItemType } from "./CartItem.types";
 
 import "./CartItem.scss";
@@ -5,8 +7,42 @@ import "./CartItem.scss";
 import useCart from "../hooks/useCart";
 
 type CartItemProps = CartItemType & {
-  itemIndex: number;
+  itemIndex?: number;
 };
+
+function BtnDecrementQty({ id }: { id: string | number }) {
+  const {decrementCartItemQty: decrement} = useCart()
+  return (
+    <button
+      aria-label="decrement quantity"
+      onClick={() => decrement && decrement(id)}
+    >
+      <FontAwesomeIcon icon={faMinus} />
+    </button>
+  );
+}
+function BtnIncrementQty({ id }: { id: string | number }) {
+  const {incrementCartItemQty: increment} = useCart()
+  return (
+    <button
+      aria-label="increment quantity"
+      onClick={() => increment && increment(id)}
+    >
+      <FontAwesomeIcon icon={faMinus} />
+    </button>
+  );
+}
+function BtnRemove({ id }: { id: string | number }) {
+  const {removeFromCart: remove} = useCart()
+  return (
+    <button
+      aria-label="remove from cart"
+      onClick={() => remove && remove(id)}
+    >
+      <FontAwesomeIcon icon={faTimes} />
+    </button>
+  );
+}
 
 function CartItem({
   id,
@@ -16,12 +52,10 @@ function CartItem({
   qty,
   thumbnail,
 }: CartItemProps) {
-  const { decrementCartItemQty, incrementCartItemQty, removeFromCart } =
-    useCart();
   return (
     <div data-testid="CartItem" className="CartItem">
       <div className="left">
-        <div className="index">{itemIndex + 1}</div>
+        {itemIndex !== undefined && <div className="index">{itemIndex + 1}</div>}
         <div
           className="thumb"
           style={{
@@ -38,36 +72,9 @@ function CartItem({
           qty: {qty}
         </div>
         <div>
-          <button
-            aria-label="decrement quantity"
-            onClick={() => {
-              if (decrementCartItemQty) {
-                decrementCartItemQty(id);
-              }
-            }}
-          >
-            -
-          </button>
-          <button
-            aria-label="increment quantity"
-            onClick={() => {
-              if (incrementCartItemQty) {
-                incrementCartItemQty(id);
-              }
-            }}
-          >
-            +
-          </button>
-          <button
-            aria-label="remove from cart"
-            onClick={() => {
-              if (removeFromCart) {
-                removeFromCart(id);
-              }
-            }}
-          >
-            X
-          </button>
+          <BtnDecrementQty id={id} />
+          <BtnIncrementQty id={id} />
+          <BtnRemove id={id} />
         </div>
       </div>
     </div>
