ec8b8870b105a592d2aebf419ade735ce6b4685b
Dov Rine <dov@techimpact.org>
2021-07-01 17:15:27 -0700

    refactor: remove zombie code

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,10 +1,8 @@
 import ProductList from './ProductList'
-import SplashPage from './SplashPage'
 
 function Shop() {
   return (
     <div data-testid='Shop' className='Shop'>
-      {/* <SplashPage /> */}
       <ProductList />
     </div>
   )
