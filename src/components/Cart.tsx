4f3f6678a5dab8e98696a253a8665697ef9942ef
Derrique B <derriquebaluyut@gmail.com>
2021-07-06 14:25:48 -0700

    fix merge conflict

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 import { useState } from "react";
 
 import { CartItemType } from "./CartItem.types";
 import CartItem from "./CartItem";
 import useCart from "../hooks/useCart";
+=======
+import { CartItemType } from './CartItem.types';
+import CartItem from './CartItem';
+import Checkout from './Checkout';
+import useCart from '../hooks/useCart';
+>>>>>>> 030198c (fix merge conflicts)
 
-import "./Cart.scss";
+import './Cart.scss';
 
 function Cart() {
   const { cartItems, calculateCart, checkout } = useCart();
@@ -17,18 +24,18 @@ function Cart() {
   const [zipcode, setZipcode] = useState("60645");
 
   return (
-    <div data-testid="Cart" className="Cart">
+    <div data-testid='Cart' className='Cart'>
       <header>
-        <h1 className="subtotal" aria-label="subtotal">
-          Your Cart{" "}
-          {new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
+        <h1 className='subtotal' aria-label='subtotal'>
+          Your Cart{' '}
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
           }).format(calculateCart())}
         </h1>
-        <h1 aria-label="item count">(Items: {cartItems?.length ?? 0})</h1>
+        <h1 aria-label='item count'>(Items: {cartItems?.length ?? 0})</h1>
       </header>
-      <div className="cart-items">
+      <div className='cart-items'>
         {cartItems?.map((item: CartItemType, i: number) => (
           <CartItem key={item.id} {...item} itemIndex={i} />
         ))}
